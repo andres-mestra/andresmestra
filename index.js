e5e56cf810c6051ca24868d42f76eab3340438bd
andresdev <andresmestra11a@gmail.com>
2021-05-12 22:42:31 -0500

    Config eslint and prettier

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 require('dotenv').config()
 const Fastify = require('fastify')
-const { PrismaClient } = require('@prisma/client')
 const mercurius = require('mercurius')
-const { makeExecutableSchema } = require('@graphql-tools/schema')
-
-
 
 const app = Fastify()
 const PORT = process.env.PORT
@@ -14,30 +10,28 @@ type Query {
   hola: String
 }
 `
-  
+
 const resolvers = {
   Query: {
-    hola: async () => "Hello world fastity and graphql"
-  }
+    hola: async () => 'Hello world fastity and graphql',
+  },
 }
 
 app.register(mercurius, {
-schema,
-resolvers,
-graphiql: 'playground',
-playgroundHeaders (window) {
-  return {
-    authorization: `bearer ${window.sessionStorage.getItem('token')}`
-  }
-}
+  schema,
+  resolvers,
+  graphiql: 'playground',
+  playgroundHeaders(window) {
+    return {
+      authorization: `bearer ${window.sessionStorage.getItem('token')}`,
+    }
+  },
 })
 
-
-app.listen(PORT)
-.then(address => console.log(`server listening on ${address} 🚀`))
-.catch((error) => {
+app
+  .listen(PORT)
+  .then((address) => console.log(`server listening on ${address} 🚀`))
+  .catch((error) => {
     console.log('Error starting server:', error)
     process.exit(1)
-})
-
-
+  })
